e58ba1ca526c8c601ab212bc5953fa0bfa08de39
JALA MEDIA COMP <maryatulkippiyah01@gmail.com>
2022-05-26 21:10:29 +0700

    percobaan Thread Runnable

---
+++ app/src/main/java/com/maryatul/threadrunnable/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.maryatul.threadrunnable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.AsyncTask
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Worker().execute()
+        }
+    }
+    inner class Worker : AsyncTask<Void, String, Boolean>() {
+        override fun doInBackground(vararg p0: Void?): Boolean {
+            for (i in 1..20) {
+                publishProgress(i.toString())
+                Thread.sleep(2000)
+            }
+            return true
+        }
+        override fun onProgressUpdate(vararg values: String?) {
+            textView.text = values[0]
+        }
+        override fun onPostExecute(result: Boolean?) {
+            println(result)
+        }
+    }
+}
